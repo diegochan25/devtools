25451a99b31d37c9a5982b4b5aed397008376ae9
diegochan25 <xdiego.chanx@gmail.com>
2025-11-10 15:15:42 -0500

    Core lib rewrite v0.3.1 (Additional JS and PY functionalities)

--- src/templates/js/lambda.py
+++ src/templates/js/lambda.py
@@ -2,17 +2,7 @@ from src.config.rule_set import JavaScriptRules
 from src.core.decorators import requires
 from src.core.template import Template
 
+
+
 def js_lambda(**kwargs) -> Template:
-    js = JavaScriptRules.generate()
-    return Template(
-        filename='index.js',
-        contents= [
-            f"export const handler = async {js.arr_fn_pl}{js.event}{js.es8_c}{js.arr_fn_pr} =>{js.blck_s}{{",
-            f"{js.t}const response = {{",
-            f"{js.t * 2}statusCode: 200,",
-            f"{js.t * 2}body: JSON.stringify({js.q}Hello from Lambda!{js.q}){js.es5_c}",
-            f"{js.t}}}{js.semi}",
-            f"{js.t}return response{js.semi}",
-            f"}}{js.semi}"
-        ]
-    )
+    pass
